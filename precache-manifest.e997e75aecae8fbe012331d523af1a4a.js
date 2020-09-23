61c8f5606ba7b094e25fc41063d9e2063fda221f
kusdiawan <71364605+kusdiawan@users.noreply.github.com>
2020-09-23 16:47:37 +0700

    Add files via upload

---
+++ precache-manifest.e997e75aecae8fbe012331d523af1a4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f4265b533b3b4ff73efb84f7e86f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e911371e0000a6207a10",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "045d413b3fed68ed8726",
+    "url": "/static/js/2.0a0998b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0a0998b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e911371e0000a6207a10",
+    "url": "/static/js/main.9cdf1205.chunk.js"
+  },
+  {
+    "revision": "23363dbf6347cae36a63",
+    "url": "/static/js/runtime-main.a13e9720.js"
+  }
+]);
